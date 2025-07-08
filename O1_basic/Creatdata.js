37c31871aa5fe5094d74a8faccfa5680cf3a97b2
Ravishankar Singh <ravipatelvns714@gmail.com>
2025-07-08 05:07:26 +0000

    commit

---
+++ O1_basic/Creatdata.js
@@ -0,0 +1,9 @@
+const accountId=22933
+let accountEmail="ravipatelvns714@gmail.com"
+var accountPassword="123883"
+ accountCity="Jaipur"
+
+
+//  console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
