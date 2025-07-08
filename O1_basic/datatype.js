b8698e3be25a1a71eb840472e8b4e4c8ee974a14
Ravishankar Singh <ravipatelvns714@gmail.com>
2025-07-08 05:24:31 +0000

    Data_type

---
+++ O1_basic/datatype.js
@@ -0,0 +1,28 @@
+/*
+Prefer not to use var because of issue in block
+Scope and functional space
+*/
+
+"use Strict";// treat all Js code an newer Version 
+// alert("hello")  we are usine nose js not browser
+
+console.log(3
+    +3//    code readbility should be high
+)
+
+let name="Ravi"
+let age=20
+let isLoggedIn=false
+
+console.table([name, age, isLoggedIn])
+
+// number => 2 to power 53
+//bigint 
+// String=> ""
+//boolean => true/false
+//null=> standalone value, 
+// undefine=>
+//Symbol=> unique
+//Object
+
+console.log(typeof null)
